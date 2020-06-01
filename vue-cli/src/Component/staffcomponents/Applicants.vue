3f95a4cfb0f9bfc20102ce767a05cd0ccb1c0246
ngocdo4299 <47731206+ngocdo4299@users.noreply.github.com>
2020-06-02 01:22:06 +0700

    vuetify#2

--- vue-cli/src/Component/staffcomponents/Applicants.vue
+++ vue-cli/src/Component/staffcomponents/Applicants.vue
@@ -1,106 +1,97 @@
 <template>
- <v-container fluid style="margin: 0px; padding: 0px; width: 100%; height: 90vh" >
-          <v-col>
-              <div class = "action-block">Filter area</div>
-              <div class= "applicant-list"> 
-                    <div class = "applicant" v-for="i in 100" @click="applicantInfor = i">
+  <v-container fluid style="margin: 0px; padding: 0px; width: 100%; height: 90vh">
+    <v-col>
+      <div class="action-block">Filter area</div>
+      <div class="applicant-list">
+        <v-list shaped>
+          <v-list-item-group>
+            <v-list-item
+              v-for="i in 100"
+              :key="i"
+              @click="applicantInfor = i"
+              v-slot:default="{ active, toggle }"
+            >
+              <template :color="active ? undefined : 'grey lighten-1'" @click="toggle">
+                <v-list-item-action>
+                  <v-checkbox color="primary"></v-checkbox>
+                </v-list-item-action>
 
-                        <div class="checkboxContainer">
-                            <input class = "idCheckbox" type="checkbox"/>
-                        </div>
+                <v-list-item-content>
+                  <v-list-item-title>Applicant {{i}}</v-list-item-title>
+                  <v-list-item-subtitle>Applicants Infor</v-list-item-subtitle>
+                </v-list-item-content>
 
-                        <div class="infoApp">
-                                <span>Applicant list view | {{i}}</span>
-                                <p>20-1-2020 | Female | ICT</p>
-                        </div>
-
-                        <div class="status-view">
-                            Status: Not approved
-                        </div>
-
-                    </div>
-                </div>
-          </v-col>
-          <v-divider></v-divider>
-          <v-col>
-              <div class= "applicant-view"> 
-           <div class="view-content">
-           This is the view of applicant with ID {{applicantInfor}}
-           </div>
-        </div>
-        <div class = "action-block">
-        </div>
-          </v-col>
-     </v-container>
+                <v-list-item-content>
+                  <v-list-item-title>Status:</v-list-item-title>
+                  <v-list-item-subtitle>Not Approved</v-list-item-subtitle>
+                </v-list-item-content>
+              </template>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </div>
+    </v-col>
+    <v-divider></v-divider>
+    <v-col>
+      <div class="applicant-view">
+        <div class="view-content">This is the view of applicant with ID {{applicantInfor}}</div>
+      </div>
+      <div class="action-block">
+        <v-card flat class="py-12">
+          <v-card-text>
+            <v-row>
+              <v-btn-toggle v-model="toggle_exclusive" rounded>
+                <v-btn>
+                  Approve
+                </v-btn>
+                <v-btn>
+                  Reject
+                </v-btn>
+                <v-btn>
+                  Pending
+                </v-btn>
+              </v-btn-toggle>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </div>
+    </v-col>
+  </v-container>
 </template>
 <script>
 export default {
-    data: function(){
-        return {
-            applicantInfor: null
-        }
-}
-}
+  data: function() {
+    return {
+      applicantInfor: null
+    };
+  }
+};
 </script>
 <style scoped>
 .col {
-    float: left;
-    width: 50%;
-    height: 100%;
+  float: left;
+  width: 50%;
+  height: 100%;
 }
 .applicant-list {
-    width: auto;
-    height: 92%;
-    overflow-y: scroll ;
-    background-color: rgb(255, 255, 255);
-}
-.applicant {
-    font-family: Tahoma;
-    box-sizing: border-box;
-    height: 10%;
-    display: table;
-    width: 100%;
-    border-bottom: 0.5px solid rgb(66, 66, 66);
-}
-.infoApp {
-    padding: 0.5vh 0;
-}
-span {
-  display: table-row;
-  font-size: 2vh;
-}
-p {
-  display: table-row;
-  font-size: 1.5vh;
-}
-
-.applicant:hover {
-  background-color: rgb(224, 224, 224);
-}
-
-.checkboxContainer {
-    display: table-cell;
-    padding: 1vh 0;
+  width: auto;
+  height: 92%;
+  overflow-y: scroll;
+  background-color: rgb(255, 255, 255);
 }
 .applicant-view {
-    width: auto;
-    height: 92%;
-    overflow-y: scroll ;
-    background-color: rgb(175, 210, 255);
+  width: auto;
+  height: 92%;
+  overflow-y: scroll;
+  background-color: rgb(175, 210, 255);
 }
 .view-content {
-    padding: 3%;
+  padding: 3%;
 }
 .action-block {
-    position: sticky;
-    width: 100%;
-    height: 8%;
-    background-color: blanchedalmond;
-}
-
-.status-view {
-    display: table-cell;
-     padding: 0.5vh 0;
-     font-size: 1.5vh;
+  position: sticky;
+  width: 100%;
+  height: 8%;
+  background-color: blanchedalmond;
 }
 </style>
